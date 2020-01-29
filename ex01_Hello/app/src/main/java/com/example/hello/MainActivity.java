cefa700372f07d097cb0b3cec959720b283f0c12
moon maria <56239469+kha0213@users.noreply.github.com>
2020-01-29 21:40:54 +0900

    11

--- ex01_Hello/app/src/main/java/com/example/hello/MainActivity.java
+++ ex01_Hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.hello;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onButton1Clicked(View v){
+        Toast.makeText(getApplicationContext(), "버튼이 눌렸어요.",Toast.LENGTH_LONG).show();
+    }
+    //Toast = 잠깐 떳다가 사라지는 메세지
+    public void onButton2Clicked(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+    //Intent
 }
